5ab70d6e8e83a7e085ac03c2cad1ce3244ee5712
rika <rika_4444@yahoo.com>
2019-04-11 00:08:46 +0700

    Initialize index.hmtl and stringmatch.py

---
+++ backend/stringmatch.py
@@ -0,0 +1,121 @@
+import re
+
+#Algoritma KMP
+def KnuthMorrisPratt(text, pattern): #text, pattern = string
+    t = len(text)
+    p = len(pattern)
+    #lps = longest prefix suffix
+    lps = [0 for i in range(t)]
+    countLongestPrefSuf(lps, pattern, p)
+
+    i = 0; j = 0
+    found_at_least_one = False
+    while i < t:
+        if (pattern[j] == text[i]):
+            i += 1
+            j += 1
+
+        if (j == p):
+            #pattern ditemukan
+            print("pattern ada di", (i-j))
+            found_at_least_one = True
+            j = lps[j-1]
+        elif (i < t) and (pattern[j] != text[i]):
+            if (j != 0):
+                j = lps[j-1]
+            else:
+                i += 1
+    if (not found_at_least_one):
+        print("pattern tidak ditemukan")
+
+def countLongestPrefSuf(lps, pattern, len_of_pattern):
+    temp_len = 0
+
+    lps[0] = 0
+    
+    i = 1
+    while (i < len_of_pattern):
+        if (pattern[i] == pattern[temp_len]):
+            temp_len += 1
+            lps[i] = temp_len
+            i += 1
+        else:
+            if (temp_len != 0):
+                temp_len = lps[temp_len-1]
+            else:
+                lps[i] = 0
+                i += 1
+
+#Algoritma Boyer-Moore
+def BoyerMoore(text, pattern): #text, pattern = string
+    last = LastOccurenceFunction(pattern)
+    t = len(text)
+    p = len(pattern)
+    i = p-1 # where matching starts
+    if (i > t-1):
+        return False
+
+    j = p-1
+    while (i <= t-1):
+        if (pattern[j] == text[i]):
+            if (j == 0):
+                return True
+            else:
+                j -= 1
+                i -= 1
+        else:
+            last_occurence = last[ord(text[i])]
+            i += (p - min(j, (last_occurence + 1)))
+
+    return False
+    
+def LastOccurenceFunction(pattern): #pattern = string
+    last = [-1 for _ in range(256)] # 256 as in ASCII
+
+    for i in range(len(pattern)):
+        last[ord(pattern[i])] = i
+
+    return last
+
+
+def check(textIn, textDB):
+    similarity = 0
+    for i in textDB:
+        if (i in textIn):
+            similarity += 1
+
+    return (similarity/len(textDB))*100
+
+
+question_dict = {
+    "kamu jeleg" : "pepega",
+    "aku siap" : "uwah",
+    "aku tidak siap" : "men",
+    "kenapa kau" : "aing cool",
+    "tapi kan" : "watashi sasuke desu",
+    "keluar" : "Bye bye"
+}
+if __name__ == '__main__':
+    while (True):
+        u = input(">>> ")
+        if (u == "keluar"):
+            print(question_dict[u])
+            break
+        else:
+            sudah = False
+            sim = []
+            for i, val in question_dict.items():
+                if (BoyerMoore(u, i)):
+                    print(val)
+                    sudah = True
+                    break
+                else:
+                    sim.append([check(u, i), i])
+
+            if (not sudah):
+                sim.sort()
+                print("not found, mungkin pake ini:")
+                for i in range(-1, -4, -1):
+                    print("-", sim[i][1])
+                    # print("-", sim[-2][1])
+                    # print("-", sim[-3][1])
