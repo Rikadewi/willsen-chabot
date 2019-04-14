bdf4171308e76d7fe77b78363356e23580366318
rika <rika_4444@yahoo.com>
2019-04-15 02:30:01 +0700

    Add startscreen and make message component

--- frontend/App.js
+++ frontend/App.js
@@ -1,13 +1,6 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, TextInput, View, Image, TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Image, TouchableHighlight, Button } from 'react-native';
 import {createStackNavigator, createAppContainer} from 'react-navigation';
-
-const MainNavigator = createStackNavigator({
-  Home: {screen: HomeScreen},
-  Exit: {screen: ExitScreen},
-});
-
-const App = createAppContainer(MainNavigator);
 
 const URL = 'http://localhost:8000/stringmatch/';
 
@@ -15,21 +8,18 @@ const postQuestion = (raw) => {
   let src = raw.toLowerCase().trim();  
   console.log(src);
   return fetch(URL, {
-      method: 'POST',
-      body: JSON.stringify(src)
-    })
-    .then((res) => res.json())
-    .catch((error) => {
-      console.log('There has been a problem with your fetch operation: ' + error.message);
-      throw error;
-    });
+    method: 'POST',
+    body: JSON.stringify(src)
+  })
+  .then((res) => res.json())
+  .catch((error) => {
+    console.log('There has been a problem with your fetch operation: ' + error.message);
+    throw error;
+  });
 }
 
-class HomeScreen extends Component {
-  static navigationsOptions = {
-    title: 'Welcome',
-  };
 
+class Message extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -38,44 +28,75 @@ class HomeScreen extends Component {
     };
     this.handleSubmit = this.handleSubmit.bind(this);
   };
-
+  
   handleSubmit(){
-    postQuestion(this.state.text)
+    if(this.state.text.toLowerCase().trim() == "keluar"){
+      this.props.navigation.navigate('Exit');
+    }else{
+      postQuestion(this.state.text)
       .then((res) => {
         console.log(res);
         this.setState({
           result: res,
         })
-      })
-      .catch((error)=>{
-        console.log("Api call error");
-        alert(error.message);
-      });
-  };
-
+        })
+        .catch((error)=>{
+          console.log("Api call error");
+          alert(error.message);
+        });
+    };
+  }
+    
   render() {
     const {navigate} = this.props.navigation;
     return (
-      <View style = {styles.container}>
-        <Image 
-          source = {require('./assets/stickman.gif')} 
-        />
-        <Text>Hello</Text>
+      <View>
         <Text>{this.state.result}</Text>
         <TextInput 
           style = {styles.textInputStyle}
           placeholder = "Chat with me"
           onChangeText = {(text) => this.setState({text})}
-        />
+          />
         <TouchableHighlight
           onPress = {this.handleSubmit}
-        >
+          >
           <Text>Press Me</Text>
         </TouchableHighlight>
-        <Button 
-          onPress={() => navigate('Exit')}
-        />
       </View>
+    );
+  }
+}
+
+class HomeScreen extends Component {
+  constructor(props){
+    super(props);
+  }
+  render() {
+    return(
+      <View>
+        <Image 
+          source = {require('./assets/stickman.gif')} 
+        />
+        <Text>Hello</Text>
+        <Message navigation = {this.props.navigation} />
+        <Message navigation = {this.props.navigation} />
+      </View>
+    );
+  }
+}
+
+class StartScreen extends Component {
+  static navigationsOptions = {
+    title: 'Welcome',
+  };
+  
+  render(){
+    const {navigate} = this.props.navigation;
+    return(
+      <Button 
+        title = "Start"
+        onPress = {() => navigate('Home')}
+      />
     );
   }
 }
@@ -86,7 +107,7 @@ class ExitScreen extends Component {
       <View>
         <Image 
           source = {require('./assets/stickman.gif')} 
-        />
+          />
         <Text>Bye</Text>
       </View>
     );
@@ -104,5 +125,12 @@ const styles = StyleSheet.create({
     height: 40,
   },
 });
+
+const MainNavigator = createStackNavigator({
+  Home: {screen: HomeScreen},
+  Exit: {screen: ExitScreen},
+});
+
+const App = createAppContainer(MainNavigator);
 
 export default App;
